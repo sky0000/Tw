305102ebe0d2212c4976a201508fb28e3ab17874
sky0000 <andrei.damoc@gmail.com>
2015-04-20 22:33:32 +0300

    v 0.69

---
+++ includes/menu.php
@@ -0,0 +1,16 @@
+    <nav class="menu">
+        <ul class="clearfix">
+ 
+            <li>
+                <a href="#">Cont <span class="arrow">&#9660;</span></a>
+ 
+                <ul class="sub-menu">
+                    <li><a href="profil.php">Profil</a></li>
+                    <li><a href="voucher.php">Vouchere</a></li>
+                    <li><a href="comanda.php">Comenzi</a></li>     
+                    <li><a href="dasboard.php">Dashboard</a></li>    
+                </ul>
+            </li>
+            <li><a href="login.php">Logout</a></li>
+        </ul>
+    </nav>
